60176ec4866490eb5da1eb46b3fb46182707d5b5
DcorpProj <dimabobkov2016@gmail.com>
2025-01-23 18:50:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wixp_lib',
-    version='0.0.3',
+    version='0.0.4',
     description='Created for convenience',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
